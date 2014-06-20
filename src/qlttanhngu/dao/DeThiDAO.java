c97c220aab2bcd9c979e33e04ebb32bd2ad5060b
EvilZ <EvilZ@EvilZ-PC.lan>
2014-06-20 22:05:07 +0700

    Frame Đề thi new 20/6/2014

--- src/qlttanhngu/dao/DeThiDAO.java
+++ src/qlttanhngu/dao/DeThiDAO.java
@@ -6,39 +6,84 @@
 
 package qlttanhngu.dao;
 
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import qlttanhngu.connection.DataBase;
+import qlttanhngu.dto.DeThiDTO;
 
 /**
  *
  * @author EvilZ
  */
 public class DeThiDAO extends DataBase{
+    
     public DeThiDAO() throws Exception{
         super();
     }
-//    public List<KhoaHocDTO> loadListKhoaHoc() throws Exception {
-//        List<KhoaHocDTO> lstKH = new ArrayList<KhoaHocDTO>();
-//        KhoaHocDTO khoahoc_dto = null;
-//        ResultSet resultSet = null;
-//        int i = 0;
-//        
-//        resultSet = this.executeQuery("{call LayDanhSachKhoaHoc()}");
-//        try {
-//            while(resultSet.next()){
-//                khoahoc_dto = new KhoaHocDTO();
-//                
-//                khoahoc_dto.setMaKhoaHoc(resultSet.getString(1));
-//                khoahoc_dto.setTenKhoaHoc(resultSet.getString(2));
-//                khoahoc_dto.setNgayKhaiGiang(resultSet.getDate(3));
-//                
-//                lstKH.add(khoahoc_dto);
-//                i++;
-//            }    
-//        } catch (SQLException ex) {
-//            Logger.getLogger(HocVienDAO.class.getName()).log(Level.SEVERE, null, ex);
-//        }finally{
-//            this.closeConnection();
-//        }
-//        return lstKH;
-//    }
+    
+    public List<DeThiDTO> loadListDeThi() throws Exception {
+        List<DeThiDTO> lstDT = new ArrayList<DeThiDTO>();
+        DeThiDTO dethidto = null;
+        ResultSet resultSet = null;
+        int i = 0;
+        
+        resultSet = this.executeQuery("{call LayDanhSachDeThi()}");
+        try {
+            while(resultSet.next()){
+                dethidto = new DeThiDTO();
+                dethidto.setMaDeThi(resultSet.getString(1));
+                dethidto.setLoaiDeThi(resultSet.getString(2));
+                dethidto.setTrinhDo(resultSet.getString(3));
+                dethidto.setNgayCap(resultSet.getDate(4));
+                lstDT.add(dethidto);
+                i++;
+            }    
+        } catch (SQLException ex) {
+            Logger.getLogger(HocVienDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            this.closeConnection();
+        }
+        return lstDT;
+    }
+    
+    public boolean addDeThi(DeThiDTO dethidto){
+        int resultSet = 0;
+        CallableStatement callableStatement = null;
+        try {
+            callableStatement = this.getConnection().prepareCall("{call "
+                    + "ThemDeThi(?,?,?)}");
+            callableStatement.setString(1, dethidto.getLoaiDeThi());
+            callableStatement.setString(2, dethidto.getTrinhDo());
+            callableStatement.setDate(3, dethidto.getNgayCap());
+            
+            resultSet = this.executeQueryUpdate(this.getConnection(), callableStatement);
+            if(resultSet != 0)
+               return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(LoaiDeThiDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+    
+    public boolean deleteDeThi(DeThiDTO dethidto){
+        int resultSet = 0;
+        CallableStatement callableStatement = null;
+        try {
+            callableStatement = this.getConnection().prepareCall("{call "
+                    + "XoaDeThi(?)}");
+            callableStatement.setString(1, dethidto.getMaDeThi());
+            
+            resultSet = this.executeQueryUpdate(this.getConnection(), callableStatement);
+            if(resultSet != 0)
+               return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(LoaiDeThiDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
 }
